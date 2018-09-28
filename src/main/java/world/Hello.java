074c071720da8b53749d59960f270535f2673fbd
yves <yves.dufournaud@gmail.com>
2018-09-28 22:25:37 +0200

    [jaeger] - first trace - sl4j

--- src/main/java/world/Hello.java
+++ src/main/java/world/Hello.java
@@ -3,8 +3,21 @@ package world;
 import io.opentracing.Span;
 import io.opentracing.util.GlobalTracer;
 
+import com.uber.jaeger.Configuration;
+import com.uber.jaeger.Configuration.ReporterConfiguration;
+import com.uber.jaeger.Configuration.SamplerConfiguration;
+
+import com.uber.jaeger.Tracer;
+
 public class Hello {
     private final io.opentracing.Tracer tracer;
+
+    public static com.uber.jaeger.Tracer initTracer(String service) {
+        SamplerConfiguration samplerConfig = new SamplerConfiguration("const", 1);
+        ReporterConfiguration reporterConfig = new ReporterConfiguration(true, null, null, null, null);
+        Configuration config = new Configuration(service, samplerConfig, reporterConfig);
+        return (com.uber.jaeger.Tracer) config.getTracer();
+    }
 
     private Hello(io.opentracing.Tracer tracer) {
         this.tracer = tracer;
@@ -20,7 +33,11 @@ public class Hello {
     }
 
     public static void main(String[] args) {
-        new Hello(GlobalTracer.get())
+        Tracer tracer = initTracer("hello-world");
+
+        new Hello( tracer)
             .sayHello("world");
+
+        tracer.close();
     }
 }
