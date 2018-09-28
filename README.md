9172254734376e5d3e29ca7d388cbe9fac454667
yves <yves.dufournaud@gmail.com>
2018-09-28 22:11:45 +0200

    [jaeger] - global nop tracer

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Maven/Java Hello world
 Execute with
 java -cp target/hello-1.0.0.jar World.Hello
 
+Jaeger integration based on https://github.com/yurishkuro/opentracing-tutorial/tree/master/java
--- src/main/java/world/Hello.java
+++ src/main/java/world/Hello.java
@@ -1,8 +1,26 @@
 package world;
 
+import io.opentracing.Span;
+import io.opentracing.util.GlobalTracer;
+
 public class Hello {
+    private final io.opentracing.Tracer tracer;
+
+    private Hello(io.opentracing.Tracer tracer) {
+        this.tracer = tracer;
+    }
+
+    private void sayHello(String helloTo) {
+        Span span = tracer.buildSpan("say-hello").startManual();
+
+        String helloStr = String.format("Hello, %s!", helloTo);
+        System.out.println(helloStr);
+
+        span.finish();
+    }
+
     public static void main(String[] args) {
-        String str = String.format("Hello, %s!", "world");
-        System.out.println( str);
+        new Hello(GlobalTracer.get())
+            .sayHello("world");
     }
 }
