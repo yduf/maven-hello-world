b19a00bf4c69e9db375b63ffdf117c0f66d78818
yves <yves.dufournaud@gmail.com>
2018-09-28 20:47:22 +0200

    [init] - first version

---
+++ README.md
@@ -0,0 +1,5 @@
+Maven/Java Hello world
+
+Execute with
+java -cp target/hello-1.0.0.jar World.Hello
+
---
+++ src/main/java/world/Hello.java
@@ -0,0 +1,8 @@
+package world;
+
+public class Hello {
+    public static void main(String[] args) {
+        String str = String.format("Hello, %s!", "world");
+        System.out.println( str);
+    }
+}
